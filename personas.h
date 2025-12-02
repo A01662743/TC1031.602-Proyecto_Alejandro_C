b3b6fe63a8c14a5c6f4306164b05ac988fc4675d
Alejandro Contreras <A01662743@tec.mx>
2025-12-02 15:46:43 -0600

    update Personas.h

--- personas.h
+++ personas.h
@@ -1,160 +1,328 @@
-/*
- * AVL.h
- *
- *  Created on: 11/10/2021
- *      Author: bvaldesa
- */
-
-#ifndef AVL_H_
-#define AVL_H_
-
+#include <iostream>
 #include <string>
 #include <sstream>
-#include <fstream>
-#include "personas.h"
-#include <iostream>
-
 using namespace std;
 
+#ifndef PERSONA
+#define PERSONA
 
-class AVL {
-private:
-	Persona *root;
+class AVL;
 
-public:
-	AVL();
-	~AVL();
-	bool empty() const;
-	void add(string, double);
-	bool find(double) const;
-	void remove(double);
-	void removeAll();
-	string inorder() const;
-	string preorder() const;
-	void loadFromFile(string fileName);
+class Persona{
+    private:
+        string nombre = "N/A";
+        double influencia = 0.00;
+        Persona *left = NULL, *right = NULL;
+	    int level = 0, balance = 0;
+	    Persona* predecesor();
+    public:
+        Persona();
+        Persona(string n, double i);
+        Persona(string n);
+        void print();
+        string get_nombre() const;
+        double get_influencia() const;
+        void set_nombre(string n);
+        void set_influencia(double i);
+        void add(string, double);
+        bool find(double);
+        void remove(double);
+        void removeChilds();
+        void inorder(stringstream&) const;
+        void preorder(stringstream&) const;
+        int max_depth();
+        Persona* check_tree(double*, Persona*, bool*);
+        Persona* balance_tree();
+        Persona* rot_left(Persona* );
+        Persona* rot_right(Persona* );
+        Persona* rot_left_right(Persona* );
+        Persona* rot_right_left(Persona* );
+        friend class AVL;
 };
 
-AVL::AVL() : root(0) {}
+Persona::Persona(){}
 
-AVL::~AVL() {
-	removeAll();
+Persona::Persona(string n, double i){
+    nombre = n;
+    influencia = i;
+}
+
+Persona::Persona(string n){
+    nombre = n;
+    influencia = 0.00;
+}
+
+void Persona::print(){
+    cout << nombre << " " << influencia << "%" << endl;
+}
+
+string Persona::get_nombre() const{
+    return nombre;
+}
+
+double Persona::get_influencia() const{
+    return influencia;
+}
+
+void Persona::set_nombre(string n){
+    nombre = n;
+}
+
+void Persona::set_influencia(double i){
+    influencia = i;
 }
 
  
-bool AVL::empty() const {
-	return (root == 0);
-}
-
-void AVL::add(string nam, double val) {
-	if (root != 0) {
-		if (!root->find(val)) {
-			Persona  *temp;
-			double check_val = false;
-			bool aux = false;
-			root->add(nam, val);
-			root->max_depth();
-			temp = root->check_tree(&check_val, 0, &aux);
-			if (check_val == root->influencia){
-				root = temp;
-			}
-			root->max_depth();
-		}
-	} else {
-		root = new Persona(nam, val);
-	}
-}
- 
-void AVL::remove(double val) {
-	if (root != 0) {
-		if (val == root->influencia) {
-			Persona *succ = root->predecesor();
-			if (succ != 0) {
-				succ->left = root->left;
-				succ->right = root->right;
-			}
-			delete root;
-			root = succ;
+void Persona::add(string nam, double val) {
+	if (val < influencia) {
+		if (left != 0) {
+			left->add(nam, val);
 		} else {
-			root->remove(val);
+			left = new Persona(nam, val);
 		}
-		root->max_depth();
-		Persona  *temp;
-		double check_val = false;
-		bool aux = false;
-		temp = root->check_tree(&check_val, 0, &aux);
-		if (check_val == root->influencia){
-			root = temp;
-		}
-		root->max_depth();
-	}
-}
-
- 
-void AVL::removeAll() {
-	if (root != 0) {
-		root->removeChilds();
-	}
-	delete root;
-	root = 0;
-}
-
- 
-bool AVL::find(double val) const {
-	if (root != 0) {
-		return root->find(val);
 	} else {
-		return false;
+		if (right != 0) {
+			right->add(nam, val);
+		} else {
+			right = new Persona(nam, val);
+		}
+	}
+}
+ 
+bool Persona::find(double val) {
+	if (val == influencia) {
+		return true;
+	} else if (val < influencia) {
+		return (left != 0 && left->find(val));
+	} else if (val > influencia) {
+		return (right != 0 && right->find(val));
 	}
 }
 
  
-string AVL::inorder() const {
-	stringstream aux;
+Persona* Persona::predecesor() {
+	Persona *le, *ri;
 
-	if (!empty()) {
-		root->inorder(aux);
+	le = left;
+	ri = right;
+
+	if (left == 0) {
+		if (right != 0) {
+			right = 0;
+		}
+		return ri;
 	}
-	return aux.str();
+
+	if (left->right == 0) {
+		left = left->left;
+		le->left = 0;
+		return le;
+	}
+
+	Persona *parent, *child;
+	parent = left;
+	child = left->right;
+	while (child->right != 0) {
+		parent = child;
+		child = child->right;
+	}
+	parent->right = child->left;
+	child->left = 0;
+	return child;
 }
 
  
-string AVL::preorder() const {
-	stringstream aux;
+void Persona::remove(double val) {
+	Persona * succ, *old;
 
-	aux << "[";
-	if (!empty()) {
-		root->preorder(aux);
+	if (val < influencia) {
+		if (left != 0) {
+			if (left->influencia == val) {
+				old = left;
+				succ = left->predecesor();
+				if (succ != 0) {
+					succ->left = old->left;
+					succ->right = old->right;
+				}
+				left = succ;
+				delete old;
+			} else {
+				left->remove(influencia);
+			}
+		}
+	} else if (val > influencia) {
+		if (right != 0) {
+			if (right->influencia == val) {
+				old = right;
+				succ = right->predecesor();
+				if (succ != 0) {
+					succ->left = old->left;
+					succ->right = old->right;
+				}
+				right = succ;
+				delete old;
+			} else {
+				right->remove(val);
+			}
+		}
 	}
-	aux << "]";
-	return aux.str();
 }
 
-//formato ej: Alex 50.4
-void AVL::loadFromFile(string fileName) {
-    ifstream file(fileName.c_str());
-    string line;
-    
-    if (file.is_open()) {
-        while (getline(file, line)) {
-            stringstream ss(line);
-            string nombre;
-            double influencia;
-
-            if (ss >> nombre >> influencia) {
-                if (influencia >= 0 && influencia <= 100) {
-                    this->add(nombre, influencia);
-                } else {
-                    cerr << "Fuera de rango (0-100) para " << nombre << ". Saltando registro." << endl;
-                }
-            } else {
-                cerr << "Advertencia: Formato de línea incorrecto. Saltando línea: " << line << endl;
-            }
-        }
-        file.close();
-		
-    } else {
-        cerr << "Error: No se pudo abrir el archivo " << fileName << endl;
-    }
+ 
+void Persona::removeChilds() {
+	if (left != 0) {
+		left->removeChilds();
+		delete left;
+		left = 0;
+	}
+	if (right != 0) {
+		right->removeChilds();
+		delete right;
+		right = 0;
+	}
 }
 
-#endif /* AVL_H_ */
+ 
+void Persona::inorder(stringstream &aux) const {
+	if (left != 0) {
+		left->inorder(aux);
+	}
+	aux << nombre << " " << influencia << "%" << endl;
+	if (right != 0) {
+		right->inorder(aux);
+	}
+}
+
+ 
+void Persona::preorder(stringstream &aux) const {
+	aux << "(" << nombre << " " << influencia << "% " << level << " " << balance << ")";
+	if (left != 0) {
+		aux << " ";
+		left->preorder(aux);
+	}
+	if (right != 0) {
+		aux << " ";
+		right->preorder(aux);
+	}
+}
+
+ 
+int Persona::max_depth() {
+	int le =0 , ri = 0;
+	if (left != 0)
+		le = left->max_depth() + 1;
+	if (right != 0)
+		ri = right->max_depth() + 1;
+	if(le > ri)
+		level = le;
+	else
+		level = ri;
+	balance = le - ri;
+	return level;
+}
+
+ 
+Persona* Persona::check_tree(double *check_val, Persona *parent, bool *checked) {
+	//cout<< "node  influencia " << nombre << " " << influencia <<"% "<< level <<" "<< balance << endl;
+	Persona*le = 0, *ri = 0, *a = 0;
+	if (left != 0)
+		le = left->check_tree(check_val,this,checked);
+	if (right != 0)
+		ri = right->check_tree(check_val,this,checked);
+	if (*checked == false){
+		if (balance > 1){
+			a = balance_tree();
+			*check_val = influencia;
+			*checked = true;
+			if(parent != 0){
+				//cout<< "parent  " << parent->nombre << " " << parent->influencia <<" parent->left "<< parent->left->nombre << " " << parent->left->influencia<< endl;
+				parent->left = a;
+			}
+		}else if (balance < -1){
+			a = balance_tree();
+			*check_val = influencia;
+			*checked = true;
+			if(parent != 0){
+				//cout<< "parent  " << parent->nombre << " " << parent->influencia <<" parent->right "<< parent->right->nombre << " " << parent->right->influencia<< endl;
+				parent->right = a;
+			}
+		}
+	}
+	return a;
+}
+
+ 
+Persona* Persona::balance_tree() {
+	Persona *a = this, *le =left, *ri =right;
+	int old_influencia = influencia;
+	//cout<< "Balancing node : " << a->nombre << " " << a->influencia << " " << endl;
+	if (balance > 0) {
+		if (le->balance > 0){
+			//cout<< "rot_right  " << a->balance << " left: " << le->balance << endl;
+			a = rot_right(a);
+		}else{
+			//cout<< "rot_left_right " << a->balance << " left: " << le->balance << endl;
+			a = rot_left_right(a);
+		}
+	}else{
+		if (ri->balance < 0){
+			//cout<< "rot_left " << a->balance << " right: " << ri->balance << endl;
+			a = rot_left(a);
+		}else{
+			//cout<< "rot_right_left " << a->balance << " right: " << ri->balance << endl;
+			a = rot_right_left(a);
+		}
+	}
+	//cout<< "New current node is " << a->nombre << " " << a->influencia << endl;
+	return a;
+}
+
+ 
+Persona* Persona::rot_left(Persona* a){
+	Persona *b, *temp = 0;
+	if (a->right != 0){
+		b = a->right;
+		if (b->left != 0)
+			temp = b->left;
+		b->left = a;
+		if(temp != 0){
+			a->right = temp;
+		}else{
+			a->right = 0;
+		}
+	}
+	return b;
+}
+
+ 
+Persona* Persona::rot_right(Persona* a){
+	Persona *b, *temp = 0;
+	if (a->left != 0){
+		b = a->left;
+		if (b->right != 0)
+			temp = b->right;
+		b->right = a;
+		if(temp != 0){
+			a->left = temp;
+		}else{
+			a->left = 0;
+		}
+	}
+	return b;
+}
+
+ 
+Persona* Persona::rot_left_right(Persona* a){
+	a->left = rot_left(a->left);
+	a = rot_right(a);
+	return a;
+}
+
+ 
+Persona*  Persona::rot_right_left(Persona* a){
+	a->right = rot_right(a->right);
+	a = rot_left(a);
+	return a;
+}
+
+#endif /* PERSONA */
